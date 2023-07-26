cdf36742f921d9c9a0b2a066595fe9a92b92c69f
Vladyslav Yachyn <reqvite2233@gmail.com>
2023-07-27 00:21:04 +0300

    upd

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const { errorHandler } = require("./helpers/apiHelpers");
@@ -9,6 +10,11 @@ const { categoriesRouter, userRouter } = require("./routes/api");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+const wellKnownPath = path.join(__dirname, ".well-known");
+
+// Розпізнавання запитів до директорії .well-known і повернення відповідного файлу
+app.use("/.well-known", express.static(wellKnownPath));
 
 app.use(logger(formatsLogger));
 app.use(cors());
