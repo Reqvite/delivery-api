31f4dde294787a4685cc02aab5a143d2c731a0fa
Vladyslav Yachyn <reqvite2233@gmail.com>
2023-07-26 19:43:57 +0300

    Update payStatusController.js

--- controllers/user/payStatusController.js
+++ controllers/user/payStatusController.js
@@ -1,7 +1,9 @@
 const { payStatus } = require("../../services/user/getLiqPaySignature");
 
 const payStatusController = async (req, res) => {
+  console.log(req);
   const { data, signature } = req.body;
+  console.log(1);
   console.log(req.body);
 
   payStatus(data, signature);
